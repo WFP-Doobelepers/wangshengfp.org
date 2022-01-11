f66c0f612f2b80e29bda15010c517711f67f7d90
Narin <narin.tana@hotmail.com>
2022-01-11 03:27:13 +0000

    Update disclaimer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="footer relative bottom-0 w-full bg-[#452222] p-5 text-center text-white">
         <p>
             MIT Licensed | Built using <a target="_blank" href="https://nuxtjs.org/"><img src="~/assets/icons/nuxtjs.svg" alt="NuxtJS" class="inline pb-1.5"></a> and <a target="_blank" href="https://tailwindcss.com/"><img src="~/assets/icons/tailwind.svg" alt="tailwindcss" class="inline pb-1"></a> <br>
-            Wangsheng Funeral Parlor is not associated with miHoYo.
+            Wangsheng Funeral Parlor is in no way sponsored, endorsed or administered by or associated with miHoYo Co., Ltd.
         </p>
     </div>
 </template>
