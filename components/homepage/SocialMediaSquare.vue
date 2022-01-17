432664e6ab62e8575f733253fad68f4e41f7d2ad
Cecilio <james@cecilioacc.net>
2022-01-17 12:17:17 +0800

    Create social media table for homepage

---
+++ components/homepage/SocialMediaSquare.vue
@@ -0,0 +1,41 @@
+<template>
+    <a :href="link" :class="`group max-w-[330px] max-h[330px] flex-grow after:block after:content-[''] after:pb-[100%] relative`">
+        <div class="flex absolute flex-col w-full h-full items-center pt-[7%] opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-[0.4s]">
+            <div class="w-[50%] rounded-[50%] border-white border-[6px] border-solid" :title="title">
+                <DynamicImage :path="emote" :imageclass="'rounded-[50%]'" />
+            </div>
+            <p class="text-center leading-none font-wfp whitespace-normal pt-3 pb-3 sm:text-2xl sm:pt-2 sm:pb-2 md:text-3xl md:pt-4 md:pb-4">
+                {{ name }}
+            </p>
+        </div>
+        <DynamicImage :path="icon" :imageclass="'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[45%] group-hover:top-[85%] group-hover:h-[18.18%] transition-all ease-in-out duration-[0.4s]'" />
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'SocialMediaSquare',
+    props: {
+        link: {
+            type: String,
+            required: true
+        },
+        icon: {
+            type: String,
+            required: true
+        },
+        emote: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
