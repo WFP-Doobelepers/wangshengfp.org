022c75f656983dce31ccccc1f82f5839f617db9a
Narin <narin.tana@hotmail.com>
2022-02-05 14:46:52 +0000

    Fix fonts

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <div id="container" class="flex justify-center w-full">
             <img src="~/assets/wfp_logo.svg" class="w-[30%] lg:w-4/5 h-auto">
         </div>
-        <p class="text-center leading-none font-wfp text-3xl lg:text-[80px] whitespace-normal">
+        <p class="text-center leading-none font-righteous text-3xl lg:text-[80px] whitespace-normal">
             WANGSHENG FUNERAL PARLOR
         </p>
 
