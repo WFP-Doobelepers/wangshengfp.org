1c20b74981477db7a8fc86de0f3d579130cb88ac
Narin <narin.tana@hotmail.com>
2022-06-02 21:55:23 +0100

    Update license in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="footer relative bottom-0 w-full bg-[#452222] p-5 text-center text-white font-quicksand z-10">
         <p>
-            MIT Licensed | Built using <a target="_blank" href="https://nuxtjs.org/"><img src="~/assets/icons/nuxtjs.svg" alt="NuxtJS" class="inline pb-1.5"></a> and <a target="_blank" href="https://tailwindcss.com/"><img src="~/assets/icons/tailwind.svg" alt="tailwindcss" class="inline pb-1"></a> <br>
+            Apache License 2.0 | Built using <a target="_blank" href="https://nuxtjs.org/"><img src="~/assets/icons/nuxtjs.svg" alt="NuxtJS" class="inline pb-1.5"></a> and <a target="_blank" href="https://tailwindcss.com/"><img src="~/assets/icons/tailwind.svg" alt="tailwindcss" class="inline pb-1"></a> <br>
             Wangsheng Funeral Parlor is in no way sponsored, endorsed or administered by or associated with miHoYo Co., Ltd.
         </p>
     </div>
