49f294e2b88f4cf9be6bcb612262913ee6e56714
Narin <narin.tana@hotmail.com>
2022-12-03 03:19:01 +0000

    fix

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -30,7 +30,7 @@
                     </NuxtLink>
 
                     <NuxtLink to="/faq">
-                        FAQ
+                        Pre-TC FAQs
                     </NuxtLink>
 
                     <NuxtLink to="/artists">
@@ -155,7 +155,7 @@
                             class="my-4 inline-block"
                             @click.native="isOpen = false"
                         >
-                            FAQ
+                            Pre-TC FAQs
                         </NuxtLink>
                     </li>
 
